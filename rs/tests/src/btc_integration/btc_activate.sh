4697452430b76b798517bf7720495c64526c8bd8
Nicolas Mattia <nicolas.mattia@dfinity.org>
2024-09-13 13:16:54 +0200

    wip

--- rs/tests/src/btc_integration/btc_activate.sh
+++ rs/tests/src/btc_integration/btc_activate.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Load and start the bitcoind docker image
+
+set -euo pipefail
+
 cp /config/bitcoin.conf /tmp/bitcoin.conf
 docker load -i /config/image.tar
 docker run --name=bitcoind-node -d \
